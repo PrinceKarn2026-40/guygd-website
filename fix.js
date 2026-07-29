6bf5ea2dbecd3cce99745c4a6b74c0f7e6f333f9
PrinceKarn2026-40 <libprince1999@gmail.com>
2026-07-29 10:14:44 +0200

    remove temporary debug scripts

--- fix.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let c = fs.readFileSync('client/src/pages/dashboard/admin.html', 'utf8');
-
-// Replace all escaped closing script tags inside template literals
-c = c.replaceAll('<\\/script>', '<\\/scr"+"ipt>');
-
-fs.writeFileSync('client/src/pages/dashboard/admin.html', c, 'utf8');
-console.log('done');
--- fix_logout.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let c = fs.readFileSync('client/src/pages/dashboard/admin.html', 'utf8');
-
-// Remove the red logout button from topbar
-c = c.replace(/<button onclick="logout\(\)" style="background:#ef4444;color:#fff;border:none;border-radius:8px;padding:7px 14px;font-size:0.8rem;font-weight:600;cursor:pointer;margin-left:10px;white-space:nowrap;">&#128682; Logout<\/button>/, '');
-
-fs.writeFileSync('client/src/pages/dashboard/admin.html', c, 'utf8');
-console.log('done');
